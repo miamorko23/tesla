5fe90a5bc67c7c9e3758155af86a226eb9347005
miamorko23 <christianprince.ranara@g.msuiit.edu.ph>
2023-11-20 23:29:46 +0800

    Full user details

--- main/forms.py
+++ main/forms.py
@@ -1,26 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from .models import UserWithKey
-
-
-# class RegisterForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-
-#     class Meta:
-#         model = User
-#         fields = ["username", "email", "password1", "password2"]
-
-# class RegisterForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-#     first_name = forms.CharField(max_length=30, required=True)
-#     last_name = forms.CharField(max_length=30, required=True)
-#     address = forms.CharField(max_length=255, required=True)
-#     phone_number = forms.CharField(max_length=15, required=True)
-
-#     class Meta:
-#         model = User
-#         fields = ["username", "email", "password1", "password2", "first_name", "last_name", "address", "phone_number"]
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
--- main/models.py
+++ main/models.py
@@ -12,27 +12,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + "\n" + self.description
-    
-# class UserWithKey(User):
-#     secret_key = models.CharField(max_length=100, default=secrets.token_hex(16))
 
-# class DrowsinessEvent(models.Model):
-#     user = models.ForeignKey(UserWithKey, on_delete=models.CASCADE)
-#     time = models.DateTimeField(auto_now_add=True)
-
-# class DrowsinessEvent(models.Model):
-#     STATUS_CHOICES = [
-#         ('W', 'Warning'),
-#         ('C', 'Critical'),
-#         ('B', 'Brake Activation'),
-#         ('AW', 'Alert from Warning'),
-#         ('AC', 'Alert from Critical'),
-#         ('AB', 'Alert from Brake Activation'),
-#     ]
-
-#     user = models.ForeignKey(UserWithKey, on_delete=models.CASCADE)
-#     time = models.DateTimeField(auto_now_add=True)
-#     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='W')
 
 class UserWithKey(User):
     secret_key = models.CharField(max_length=100, default=secrets.token_hex(16))
