c99e0005bcf85dcbe349e119981bea424574f126
miamorko23 <christianprince.ranara@g.msuiit.edu.ph>
2023-11-25 03:12:01 +0800

    Display Events, Update Profile

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign_up'),
-    path('user_events', views.user_events, name='user_events'),
     path('update_profile', views.update_profile, name='update_profile'),
     path('change_password/', auth_views.PasswordChangeView.as_view(template_name='main/change_password.html', success_url='/'), name='change_password'),
 ]
+
+# path('user_events', views.user_events, name='user_events'),
