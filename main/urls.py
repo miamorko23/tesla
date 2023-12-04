d6ca43ab6c8dd7d106a8018b7fb77701fb538ca4
miamorko23 <christianprince.ranara@g.msuiit.edu.ph>
2023-12-04 23:36:47 +0800

    Redirection

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
